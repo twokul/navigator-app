21922751fb9eb61cd40d005383b1197a58bfb8a0
Alex Navasardyan <alex.navasardyan@gmail.com>
2025-09-21 10:22:11 -0700

    initial commit

---
+++ .prettierrc.mjs
@@ -0,0 +1,7 @@
+const config = {
+  semi: true,
+  printWidth: 100,
+  plugins: ["prettier-plugin-tailwindcss"],
+};
+
+export default config;
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,16 +10,15 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "prettier",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ),
   {
-    ignores: [
-      'node_modules/**',
-      '.next/**',
-      'out/**',
-      'build/**',
-      '.source/**',
-      'next-env.d.ts',
-    ],
+    ignores: ["node_modules/**", ".next/**", "out/**", "build/**", ".source/**", "next-env.d.ts"],
   },
 ];
 
